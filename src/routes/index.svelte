0dfcb09bb5961dbd5954b29e0acf30943cb466ec
briantkho <briantkho@gmail.com>
2022-08-02 00:12:22 -0700

    API Initialization

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ fetch }) => {
+		const res = await fetch('./habits');
+		const habits = await res.json;
+		return {
+			props: {
+				habits
+			}
+		};
+	};
+</script>
