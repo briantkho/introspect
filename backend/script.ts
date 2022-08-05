24bd79f30467833b01ab20ce2802afef7a055a97
briantkho <briantkho@gmail.com>
2022-08-04 18:47:25 -0700

    API Initialization

---
+++ backend/script.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+interface user {
+  data: {
+    first_name: String;
+    last_name: String;
+    email: String;
+    user_name: String;
+    password: String;
+  };
+}
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      first_name: "Brian",
+      last_name: "Ho",
+      email: "briantkho@gmail.com",
+      user_name: "bho",
+      password: "admin",
+      habits: {
+        create: {},
+      },
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
