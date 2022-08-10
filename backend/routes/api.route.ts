bb3282387e4b6de2ccc5c8328a0d49637f3a3bf6
briantkho <briantkho@gmail.com>
2022-08-09 23:32:07 -0700

    File structure change

--- backend/routes/api.route.ts
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/", async (req: any, res: any, next: any) => {
-  res.send({ message: "Ok api is working 🚀" });
-});
-
-module.exports = router;
