0b49538ce20b36315f34ed016a4814cf5bfdadf0
briantkho <briantkho@gmail.com>
2023-01-01 03:00:03 -0800

    added habit class, wip

--- server/habit/habit.ts
+++ server/habit/habit.ts
@@ -1,7 +1,7 @@
 /**
  * Habit type
  */
-export type Habit = {
+export type HabitClass = {
   hid: string;
   uid: string;
   name: string;
@@ -20,3 +20,70 @@ export type HabitReflection = {
   hid: string;
   description: string;
 };
+
+type HabitParams = {
+  name: string;
+  frequencyPerWeek: number;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  status: number;
+};
+
+class Habit {
+  hid: string;
+  uid: string;
+  name: string;
+  frequencyPerWeek: number;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  status: boolean;
+
+  constructor(
+    name: string,
+    frequencyPerWeek: number,
+    description: string,
+    startDate: Date,
+    endDate: Date
+  ) {
+    this.hid = "Foo";
+    this.uid = "User";
+    this.name = name;
+    this.frequencyPerWeek = frequencyPerWeek;
+    this.description = description;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.status = true;
+  }
+
+  addHabit(
+    name: string,
+    frequencyPerWeek: number,
+    description: string,
+    startDate: Date,
+    endDate: Date
+  ): void {
+    new Habit(name, frequencyPerWeek, description, startDate, endDate);
+  }
+
+  editName() {}
+
+  editFrequency() {}
+
+  editDescription() {}
+
+  removeHabit() {}
+
+  getHabit() {
+    return this;
+  }
+
+  completeHabit() {
+    if (!this.status) {
+      this.status = false;
+    } else {
+      throw new Error("This habit has already been completed!");
+    }
+  }
+}
